6b9ea1bd9ab7d6ceb7dca97217d17eef8b756158
Eric Stover <19699277+ejstover@users.noreply.github.com>
2026-01-19 21:22:34 -0500

    Rename site to broadcast address

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'TwelveFS';
+export const SITE_TITLE = 'ff.ff.ff.ff.ff.ff';
 export const SITE_DESCRIPTION = 'Notes on networking, automation, and building things.';
